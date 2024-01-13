65704f486f18223d367f4e9f0900ca2e4ce840cd
Tuan Kiet <tuankietkaito@gmail.com>
2024-01-14 00:07:50 +0700

    first commit

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Text from "../Text"
+import "./styles.css";
+
+const index = () => {
+  return (
+    <div className='card-container'>
+      <img src="/images/image-qr-code.png" alt="QR Code" className="card-img" />
+      <Text />
+    </div>
+  )
+}
+
+export default index
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles.css";
+
+const index = () => {
+  return (
+    <div className='text-container'>
+      <div className='text-title'>Improve your front-end skills by building projects</div>
+      <div className='text-description'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+    </div>
+  )
+}
+
+export default index
