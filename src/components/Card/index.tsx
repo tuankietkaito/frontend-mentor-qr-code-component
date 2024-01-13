000f4921f40ab4c8c3130bb02c56d43a8a2ae48c
Tuan Kiet <tuankietkaito@gmail.com>
2024-01-14 00:18:13 +0700

    Modify image path

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,7 +5,7 @@ import "./styles.css";
 const index = () => {
   return (
     <div className='card-container'>
-      <img src="/images/image-qr-code.png" alt="QR Code" className="card-img" />
+      <img src="images/image-qr-code.png" alt="QR Code" className="card-img" />
       <Text />
     </div>
   )
